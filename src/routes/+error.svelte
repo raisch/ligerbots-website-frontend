71b0a330e2aa238a4cb89c242b567b3203a3e720
Rob Raisch <raisch@gmail.com>
2024-09-19 15:59:24 -0400

    added api route and default 404 page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,18 @@
+<script>
+  import App from "../App.svelte"
+  const title = "WHOOPS! Page Not Found"
+  const status = 404
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta property="og:title" content={title} />
+</svelte:head>
+
+<App>
+  <center>
+    <h2>Status: {status}</h2>
+    <h3>{title}</h3>
+    <p>Relax, we assure you that the proper authorities have been notified!</p>
+  </center>
+</App>
