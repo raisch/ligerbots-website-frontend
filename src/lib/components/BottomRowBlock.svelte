8ef66970e8f18903a3122eddaef2bfb54ce5b440
Rob Raisch <raisch@gmail.com>
2024-09-26 17:45:15 -0400

    blockerized bottom row content

---
+++ src/lib/components/BottomRowBlock.svelte
@@ -0,0 +1,12 @@
+<div class="panel panel-brag">
+  <!-- svelte-ignore a11y-img-redundant-alt -->
+  <img
+    src="http://ligerbots.4msg.net:8055/assets/2eb80fd3-8541-415d-9333-6b98f5fa1fb5"
+    alt="LigerBots Team Photo 2022"
+  />
+</div>
+<div style="text-align:center;">
+  <div class="label-blue">
+    <a href="https://www.flickr.com/photos/ligerbots/albums/72157712218381043" target="_blank">The LigerBots</a>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,8 @@
   import BlogBlock from '$lib/components/BlogBlock.svelte'
   import AnnouncementsBlock from '$lib/components/AnnouncementsBlock.svelte'
   import TwitterBlock from '$lib/components/TwitterBlock.svelte'
+  import BottomRow from '$lib/components/BottomRowBlock.svelte'
+  import BottomRowBlock from '$lib/components/BottomRowBlock.svelte'
 
   /** @type {import('./$types').PageData} */
   export let data
@@ -34,18 +36,7 @@
 
     <div class="row row-margins">
       <div class="col-xs-12">
-        <div class="panel panel-brag">
-          <!-- svelte-ignore a11y-img-redundant-alt -->
-          <img
-            src="http://ligerbots.4msg.net:8055/assets/2eb80fd3-8541-415d-9333-6b98f5fa1fb5"
-            alt="LigerBots Team Photo 2022"
-          />
-        </div>
-        <div style="text-align:center;">
-          <div class="label-blue">
-            <a href="https://www.flickr.com/photos/ligerbots/albums/72157712218381043" target="_blank">The LigerBots</a>
-          </div>
-        </div>
+        <BottomRowBlock />
       </div>
     </div>
   {/if}
