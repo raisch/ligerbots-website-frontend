83038fba1937ac43f631b90f015abb7decec1f3a
Rob Raisch <raisch@gmail.com>
2024-09-27 16:49:37 -0400

    refactored logos

--- src/lib/components/Masthead.svelte
+++ src/lib/components/Masthead.svelte
@@ -13,46 +13,69 @@
       />
     </a>
   </div>
-  <ul>
-    <li>
-      <a class="header-link" target="_blank" href="http://www.firstinspires.org/robotics/frc">
-        <img src="/images/first.svg" alt="first robotics" />
-      </a>
-    </li>
-    <li>
-      <a class="header-link" target="_blank" href="https://www.youtube.com/c/ligerbots">
-        <img src="/images/youtube.png" alt="youtube" />
-      </a>
-    </li>
-    <li>
-      <a class="header-link" target="_blank" href="https://twitter.com/ligerbots">
-        <img src="/images/twitter.png" alt="twitter" />
-      </a>
-    </li>
-    <li>
-      <a class="header-link" target="_blank" href="https://www.facebook.com/The-LigerBots-162121450506644/">
-        <img src="/images/facebook.png" alt="facebook" />
-      </a>
-    </li>
-    <li>
-      <a class="header-link" target="_blank" href="https://www.instagram.com/ligerbots_frc2877/">
-        <img src="/images/instagram.png" alt="instagram" />
-      </a>
-    </li>
-    <li>
-      <a class="header-link" target="_blank" href="https://www.flickr.com/photos/ligerbots/">
-        <img src="/images/flickr.png" alt="flickr" />
-      </a>
-    </li>
-    <li>
-      <a class="header-link" href="/sponsor-us">
-        <img
-          style="width:10%"
-          src="http://ligerbots.4msg.net:8055/assets//5bb9e371-74d4-4462-ae03-de76107749e7"
-          alt="donate"
-        />
-      </a>
-    </li>
-  </ul>
+  <div style="width:100%">
+    <ul style="left:10%">
+      <li>
+        <a class="header-link" target="_blank" href="http://www.firstinspires.org/robotics/frc">
+          <img style="width:4%" src="/images/first.svg" alt="first robotics" />
+        </a>
+      </li>
+      <li>
+        <a class="header-link" target="_blank" href="https://www.youtube.com/c/ligerbots">
+          <img
+            style="width:8%"
+            src="http://ligerbots.4msg.net:8055/assets/e46d43c9-1e15-4e34-98a5-1bb9760ff078"
+            alt="youtube"
+          />
+        </a>
+      </li>
+      <li>
+        <a class="header-link" target="_blank" href="https://x.com/ligerbots">
+          <img
+            style="width:2%"
+            src="http://ligerbots.4msg.net:8055/assets/aaebd520-32cf-4c3e-bd31-9d8a5178ee05"
+            alt="x"
+          />
+        </a>
+      </li>
+      <li>
+        <a class="header-link" target="_blank" href="https://www.facebook.com/The-LigerBots-162121450506644/">
+          <img
+            style="width:3%"
+            src="http://ligerbots.4msg.net:8055/assets/a32f4f54-b323-45ee-a624-59276a135108"
+            alt="facebook"
+          />
+        </a>
+      </li>
+      <li>
+        <a class="header-link" target="_blank" href="https://www.instagram.com/ligerbots_frc2877/">
+          <img
+            style="width:3%"
+            src="http://ligerbots.4msg.net:8055/assets/3806f78a-68c5-4b0c-8f1d-9ac04481e94c"
+            alt="instagram"
+          />
+        </a>
+      </li>
+      <li>
+        <a class="header-link" target="_blank" href="https://www.flickr.com/photos/ligerbots/">
+          <img
+            style="width:7%"
+            src="http://ligerbots.4msg.net:8055/assets/cf477ab4-9c50-417f-b8b7-a30a24e843d8"
+            alt="flickr"
+          />
+        </a>
+      </li>
+      <li>
+        &nbsp;&nbsp;
+        <a class="header-link" href="/sponsor-us">
+          <img
+            style="width:10%"
+            src="http://ligerbots.4msg.net:8055/assets//5bb9e371-74d4-4462-ae03-de76107749e7"
+            alt="donate"
+          />
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
 <!-- #masthead -->
