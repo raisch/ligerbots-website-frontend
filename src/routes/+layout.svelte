e1706511a04a90436b6b51ff8f5d3dbbe2c57fbe
Rob Raisch <raisch@gmail.com>
2024-10-03 15:05:58 -0400

    refactored App to +layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Masthead from '$lib/components/Masthead.svelte'
+  import Navbar from '$lib/components/Navbar.svelte'
+  import MainPane from '$lib/components/MainPane.svelte'
+  import Footer from '$lib/components/Footer.svelte'
+
+  /** @type {import('./$types').PageServerLoad} */
+  // import getSiteConfig from '$lib/server/site'
+  // // import getNavbarConfig from '$lib/server/navbar'
+
+  // export async function load({ fetch }) {
+  //   const site = await getSiteConfig()
+  //   // console.log(
+  //   //   `\nin +page.server.js/getSiteConfig, config: ${JSON.stringify(site)}\n`
+  //   // )
+
+  //   return { site }
+  // }
+</script>
+
+<div id="header-ghost"></div>
+<div class="container-fluid no-side-padding" id="page-container">
+  <div class="col-xs-12 no-side-padding" id="main-column">
+    <Masthead />
+    <Navbar />
+    <MainPane>
+      <slot />
+    </MainPane>
+    <Footer />
+  </div>
+</div>
