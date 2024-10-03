832145bc425b0b90e4a52a55aef1d6db3e6dbdd6
Rob Raisch <raisch@gmail.com>
2024-10-03 18:02:30 -0400

    added page title

--- src/routes/logout/+page.svelte
+++ src/routes/logout/+page.svelte
@@ -1,11 +1,18 @@
 <script>
   import { onMount } from 'svelte'
 
+  const title = 'LigerBots Logout'
+
   onMount(async () => {
     document.cookie = `auth=; path=/; max-age=0; samesite=strict`
     sessionStorage.removeItem('user')
   })
 </script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta property="og:title" content={title} />
+</svelte:head>
 
 <center>
   <div id="msg">You have been logged out.</div>
@@ -13,6 +20,7 @@
 
 <style>
   #msg {
+    font-size: 24px;
     padding: 60px;
   }
 </style>
