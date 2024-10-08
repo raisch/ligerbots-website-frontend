65578841ba476d2cab2383dffd57213e9b3f08e1
Rob Raisch <raisch@gmail.com>
2024-10-08 12:07:24 -0400

    debug

--- src/lib/components/Masthead.svelte
+++ src/lib/components/Masthead.svelte
@@ -1,6 +1,6 @@
 <script>
   import { page } from '$app/stores'
-  console.log('Masthead.svelte:', JSON.stringify(page))
+  console.log('Masthead.svelte:', JSON.stringify($page))
 </script>
 
 <script>
@@ -13,14 +13,10 @@
     <a href="/">
       <img
         id="liger-text"
-        src="http://ligerbots.4msg.net:8055/assets/c632dcdf-27dd-448c-950d-21ec6e97e7ce"
+        src="//ligerbots.4msg.net:8055/assets/c632dcdf-27dd-448c-950d-21ec6e97e7ce"
         alt="masthead text"
       />
-      <img
-        id="liger_head"
-        src="http://ligerbots.4msg.net:8055/assets/5579e4ad-0648-4819-b917-2ee06be2aad8"
-        alt="logo"
-      />
+      <img id="liger_head" src="//ligerbots.4msg.net:8055/assets/5579e4ad-0648-4819-b917-2ee06be2aad8" alt="logo" />
     </a>
   </div>
   <div style="width:100%">
@@ -34,25 +30,21 @@
         <a class="header-link" target="_blank" href="https://www.youtube.com/c/ligerbots">
           <img
             style="width:8%"
-            src="http://ligerbots.4msg.net:8055/assets/e46d43c9-1e15-4e34-98a5-1bb9760ff078"
+            src="//ligerbots.4msg.net:8055/assets/e46d43c9-1e15-4e34-98a5-1bb9760ff078"
             alt="youtube"
           />
         </a>
       </li>
       <li>
         <a class="header-link" target="_blank" href="https://x.com/ligerbots">
-          <img
-            style="width:2%"
-            src="http://ligerbots.4msg.net:8055/assets/aaebd520-32cf-4c3e-bd31-9d8a5178ee05"
-            alt="x"
-          />
+          <img style="width:2%" src="//ligerbots.4msg.net:8055/assets/aaebd520-32cf-4c3e-bd31-9d8a5178ee05" alt="x" />
         </a>
       </li>
       <li>
         <a class="header-link" target="_blank" href="https://www.facebook.com/The-LigerBots-162121450506644/">
           <img
             style="width:3%"
-            src="http://ligerbots.4msg.net:8055/assets/a32f4f54-b323-45ee-a624-59276a135108"
+            src="//ligerbots.4msg.net:8055/assets/a32f4f54-b323-45ee-a624-59276a135108"
             alt="facebook"
           />
         </a>
@@ -61,7 +53,7 @@
         <a class="header-link" target="_blank" href="https://www.instagram.com/ligerbots_frc2877/">
           <img
             style="width:3%"
-            src="http://ligerbots.4msg.net:8055/assets/3806f78a-68c5-4b0c-8f1d-9ac04481e94c"
+            src="//ligerbots.4msg.net:8055/assets/3806f78a-68c5-4b0c-8f1d-9ac04481e94c"
             alt="instagram"
           />
         </a>
@@ -70,7 +62,7 @@
         <a class="header-link" target="_blank" href="https://www.flickr.com/photos/ligerbots/">
           <img
             style="width:7%"
-            src="http://ligerbots.4msg.net:8055/assets/cf477ab4-9c50-417f-b8b7-a30a24e843d8"
+            src="//ligerbots.4msg.net:8055/assets/cf477ab4-9c50-417f-b8b7-a30a24e843d8"
             alt="flickr"
           />
         </a>
@@ -80,7 +72,7 @@
         <a class="header-link" href="/sponsor-us">
           <img
             style="width:10%"
-            src="http://ligerbots.4msg.net:8055/assets//5bb9e371-74d4-4462-ae03-de76107749e7"
+            src="//ligerbots.4msg.net:8055/assets//5bb9e371-74d4-4462-ae03-de76107749e7"
             alt="donate"
           />
         </a>
