d4e401ff2e892f603fec9e06da47bedf6fe92623
Rob Raisch <raisch@gmail.com>
2024-10-09 16:06:21 -0400

    add redirect for /assets so they work locally.

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,18 @@
 // This hook will remove the Content-Type header from the response
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle ({ event, resolve }) {
+  /*
+   * NB: On the service machine, we use nginx to redirect requests to /assets to the backend
+   * CMS before the request is passed to sveltekit. To make this work on our desktops, we need
+   * to similarly redirect these requests here. /RR 2024-10-09
+   */
+  if (event.url.pathname.startsWith('/assets')) {
+    const redirectUrl = process.env.API_URL + event.url.pathname
+    console.log(
+      `REDIRECTING assets request: ${event.url.pathname} => ${redirectUrl}`
+    )
+    return Response.redirect(redirectUrl, 301)
+  }
   return await resolve(event, {
     filterSerializedResponseHeaders: (key, value) => {
       return key.toLowerCase() === 'content-type'
