c9a2870edd6a13c876b5f8cfbac17fed3dc06d70
Rob Raisch <raisch@gmail.com>
2024-10-22 11:39:09 -0400

    cleaned up

--- src/lib/server/client.js
+++ src/lib/server/client.js
@@ -1,13 +1,7 @@
 import 'dotenv/config'
 import createDebugMessages from 'debug'
 
-import {
-  createDirectus,
-  rest,
-  graphql,
-  authentication,
-  staticToken
-} from '@directus/sdk'
+import { createDirectus, rest, graphql, staticToken } from '@directus/sdk'
 
 const debug = createDebugMessages('APP:$lib/server/client')
 
@@ -34,23 +28,13 @@ debug(`API_URL: "${API_URL}"`)
 const API_TOKEN = process.env.API_TOKEN
 debug(`API_TOKEN: "${API_TOKEN}"`)
 
-// /** @type {string|undefined} */
-// const API_USERNAME = process.env.API_USERNAME
-// debug(`API_USERNAME: "${API_USERNAME}"`)
-
-// /** @type {string|undefined} */
-// const API_PASSWORD = process.env.API_PASSWORD
-// debug(`API_PASSWORD: "${API_PASSWORD}"`)
-
 if (!(API_URL && API_TOKEN)) {
-  throw new Error(
-    'API_URL, API_TOKEN must be defined in environment variables.'
-  )
+  throw new Error('API_URL, API_TOKEN must be defined in environment variables.')
 }
 
 /** @type {string[]} */
 const CLIENT_PROPS = [
-  // DirectusClient properties
+  // DirectusClient properties used by isDirectusClient()
   'globals',
   'url',
   'with',
@@ -79,6 +63,8 @@ let client
 /**
  * Get a logged-in instance of the Directus SDK.
  *
+ * Note: client is memoized.
+ *
  * @property {string} [apiUrl=API_URL] - The URL of the Directus API.
  * @property {string} [apiToken=API_TOKEN] - The security token of the Directus API.
  *
@@ -96,30 +82,14 @@ let client
  *  )
  *  console.log(folders)
  */
-export async function getBackendClient (apiUrl = API_URL, apiToken = API_TOKEN) {
+export async function getBackendClient(apiUrl = API_URL, apiToken = API_TOKEN) {
   if (!(apiUrl && apiToken)) {
-    throw new Error(
-      'API_URL and API_TOKEN must be defined in environment variables.'
-    )
+    throw new Error('API_URL and API_TOKEN must be defined in environment variables.')
   }
   debug(`getBackendClient("${apiUrl}", "${apiToken}")`)
 
   if (!client) {
-    client = createDirectus(apiUrl)
-      .with(staticToken(apiToken))
-      .with(rest())
-      .with(graphql())
-
-    // try {
-    //   debug(`Logging into backend with "${apiToken}"`)
-    //   await client.login(apiUsername, apiPassword)
-    // } catch (err) {
-    //   throw new Error(
-    //     `Failed to log into backend with ${apiUsername}/${apiPassword}: ${JSON.stringify(
-    //       err
-    //     )}`
-    //   )
-    // }
+    client = createDirectus(apiUrl).with(staticToken(apiToken)).with(rest()).with(graphql())
 
     return client
   }
@@ -134,8 +104,6 @@ export async function getBackendClient (apiUrl = API_URL, apiToken = API_TOKEN) 
  *
  * @returns {boolean} True if the object is a DirectusClient, false otherwise.
  */
-export function isDirectusClient (client) {
-  return (
-    typeof client === 'object' && CLIENT_PROPS.every(prop => prop in client)
-  )
+export function isDirectusClient(client) {
+  return typeof client === 'object' && CLIENT_PROPS.every((prop) => prop in client)
 }
