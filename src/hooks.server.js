324562d2945dab29c4c930337c02941f03619a72
Rob Raisch <raisch@gmail.com>
2024-10-23 14:02:13 -0400

    backed out url change

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,9 @@
 // This hook will remove the Content-Type header from the response
 
-const API_URL = `${process.env.API_SCHEME}://${process.env.API_HOST}:${process.env.API_PORT}`
+const API_URL = process.env.API_URL
+if (!API_URL) {
+  throw new Error('in hooks.server.js, API_URL must be defined in environment variables.')
+}
 
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
