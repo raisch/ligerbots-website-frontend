1f9b94d989cbbad0f16f49f4190b86cbba9b27b6
Ray <88277260+R4z0rBl8D3@users.noreply.github.com>
2024-10-25 18:59:51 -0400

    Made only show published posts

--- src/routes/announcement/+page.svelte
+++ src/routes/announcement/+page.svelte
@@ -3,25 +3,27 @@
 </script>
 
 <div class="level4-heading">Announcements</div>
+<h1 style="text-align: center;">THIS IS A TEST!</h1>
 
-<!-- I know that this UI looks bad but its just for testing :) -->
-<!-- But I tried to match the style as best I could -->
+<!-- I tried to match the style as best I could :) -->
 
 {#each data.posts.post as post}
-  <div class="row page-body" style="margin-bottom: 10px; background: none;">
-    <div class="col-md-12 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-12">
-      <div class="row row-margins">
-        <div class="col-xs-12">
-          <div style="padding: 10px;" class="panel panel-sprs">
+  {#if post.status === "published"}
+    <div class="row page-body" style="margin-bottom: 10px; background: none;">
+      <div class="col-md-12 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-12">
+        <div class="row row-margins">
+          <div class="col-xs-12">
+            <div style="padding: 10px;" class="panel panel-sprs">
 
-            <a href={ `/announcement/${post.slug}` } style="font-size: 30px;">{ post.title }</a>
-            <p>{ post.publish_on }</p>
+              <a href={ `/announcement/${post.slug}` } style="font-size: 30px;">{ post.title }</a>
+              <p>{ post.publish_on }</p>
 
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  {/if}
 {/each}
 
 <br><br>
