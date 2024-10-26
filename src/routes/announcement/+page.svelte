11b8dbf4e50126dad23869287a645124fb2f8aeb
Ray <88277260+R4z0rBl8D3@users.noreply.github.com>
2024-10-26 14:36:34 -0400

    Removed the gitpod file and fixed announcements to only show announcements

--- src/routes/announcement/+page.svelte
+++ src/routes/announcement/+page.svelte
@@ -3,19 +3,19 @@
 </script>
 
 <div class="level4-heading">Announcements</div>
-<h1 style="text-align: center;">THIS IS A TEST!</h1>
+<h1 class="text-center">THIS IS A TEST!</h1>
 
 <!-- I tried to match the style as best I could :) -->
 
 {#each data.posts.post as post}
-  {#if post.status === "published"}
-    <div class="row page-body" style="margin-bottom: 10px; background: none;">
-      <div class="col-md-12 col-md-offset-0 col-sm-10 col-sm-offset-1 col-xs-12">
-        <div class="row row-margins">
+  {#if post.status === "published" && post.type === "announcement"}
+    <div class="row mb-3 bg-transparent">
+      <div class="col-md-12 offset-md-0 col-sm-10 offset-sm-1 col-xs-12">
+        <div class="row">
           <div class="col-xs-12">
-            <div style="padding: 10px;" class="panel panel-sprs">
+            <div class="p-3 panel panel-sprs" style="padding: 10px;">
 
-              <a href={ `/announcement/${post.slug}` } style="font-size: 30px;">{ post.title }</a>
+              <a href={ `/announcement/${post.slug}` } class="h1">{ post.title }</a>
               <p>{ post.publish_on }</p>
 
             </div>
