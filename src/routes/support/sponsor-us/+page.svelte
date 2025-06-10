0ebcd986ad81f0ca03233715dc8768c75cff8467
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2025-06-09 21:17:43 -0400

    signup sections, add sponsor pages

---
+++ src/routes/support/sponsor-us/+page.svelte
@@ -0,0 +1,58 @@
+<div id="fb-root"></div>
+<div class="level4-heading">Help Support Newton Students in STEM Learning and Developing Lifelong Leadership Skills</div>
+<div class="row">
+  <div class="col-sm-9 image-border-blue" style="padding-right: 0;">
+    <img style="width: 100%;" src="/images/cheering_photo_2018.jpg" />
+  </div>
+  <div class="col-sm-3">
+    <div class="panel panel-default card" style="background: none; border-width: 2px; margin-bottom: 20px;">
+      &nbsp;
+      <div class="row justify-content-center" style="margin-top: 15px; margin-left: 25px;">
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+          <input name="hosted_button_id" type="hidden" value="YL88EZ9PKRBCE" />
+          <input title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" src="/images/donate_via_paypal.svg" type="image" width="165px" />
+          <img src="https://www.paypal.com/en_US/i/scr/pixel.gif" alt="" width="1" height="1" style:border="0" />
+        </form>
+      </div>
+      <div class="row justify-content-center" style="margin-top: 15px; margin-left: 25px;">
+        <a href="/donate-by-check/"><img src="/images/donate_via_check.svg" width="165px" /></a>
+      </div>
+      <div class="row justify-content-center" style="margin-top: 37px; margin-left: 100px; transform: scale(2.2);">
+        <div class="fb-share-button" data-href="https://ligerbots.org/sponsor" data-layout="button" data-size="large" data-mobile-iframe="false">
+          <a class="fb-xfbml-parse-ignore" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fligerbots.org%2Fsponsor" target="_blank" rel="noopener noreferrer">Share on Facebook</a>
+        </div>
+      </div>
+      <div class="row justify-content-center" style="margin-top: 57px; margin-left: 100px; transform: scale(2.2);">
+        <a class="twitter-share-button" href="https://twitter.com/share?ref_src=twsrc%5Etfw&amp;text=Help%20send%20the%20LigerBots%20to%20the%20Detroit%20FRC%20World%20Championship%21%20%20The%20team%20has%20only%20a%20few%20days%20to%20raise%20%2415k%20for%20fees%2C%20transportation%20and%20lodging.%20Please%20retweet%20%26%20share%20the%20link%20w%2F%20companies%20and%20individuals.%20%23omgrobots%20%23NEFirst%20Learn%20more%20and%20donate" data-size="large" data-show-count="false">Tweet</a>
+      </div>
+      <div class="row justify-content-center" style="margin-top: 25px; margin-bottom: 15px; margin-left: 25px;">
+        <a href="/cdn-cgi/l/email-protection#112e6264737b7472652c59747d613423216579743423215d78767463537e6562342321767465342321657e3423216579743423215279707c61787e7f627978616237707c612a737e75682c52707f342321687e6434232179747d6134232162747f753423216579743423215d78767463537e6562342321657e342321657974342321557465637e7865342321467e637d753423215279707c61787e7f627978613422573423214579743423215d78767463537e65623423217063743423215f7466657e7f34232662342321637e737e657872623423216574707c3423523423216679787279342321727e7c6174657462342321787f342321657974342321707f7f64707d3423215758434245342321437e737e65787262342321527e7c6174657865787e7f3423523423217364787d75787f7634232170342321727e7c617d7469342321637e737e6534232165797065342321617d70686234232170342321757877777463747f6534232176707c743423217470727934232168747063342352342321707f75342321777e72646274623423217e7f342321787f62617863787f76342321707f342321787f657463746265342321787f3423214245545c342321707f7534232161637e7b7472653c73706274753423217d7470637f787f763423216579637e6476797e64653423216579743423215f7466657e7f342321727e7c7c647f7865683f342321457974683423217b6462653423217d7470637f747534232165797468342321667463743423216064707d7877787475342321777e63342321657974342321467e637d753423215279707c61787e7f627978613423217d706265342321427065646375706834235234232163707f7a787f7634232123256579342321707c7e7f763423212321283423216574707c62342321787f3423215f7466342321547f767d707f753f34232145797468342321797067743423217e7f7d683423217034232177746634232175706862342321657e342321637078627434232134232520245a342321777e63342321777474623423523423216563707f62617e63657065787e7f342321707f753423217d7e7576787f763f342321417d747062743423217b7e787f3423217c74342321787f34232179747d61787f763423215f7466657e7f3454233429213428286234232170667063753c66787f7f787f76342321797876793423216272797e7e7d3423215758434245342321437e737e657872623423216574707c3f342321487e6434232172707f3423217d7470637f3423217c7e6374342321707f75342321757e7f706574342321706534232179656561623422503423573423577d78767463737e65623f7e637634235762617e7f627e633f342321417d747062743423216279706374342321657978623423217c746262707674342321667865793423217e65797463342321626461617e63657463623423217e773423214245545c342352342321707f75342321776378747f7562342321707f7534232177707c787d683f">
+            <img src="/images/share_by_email.svg" width="165px" />
+        </a>
+      </div>
+      &nbsp; &nbsp;
+    </div>
+  </div>
+</div>
+<p>
+  The LigerBots are Newton's all-city FIRST Robotics Competition (FRC) team composed of members from both Newton North and South High Schools. During competition season, students design and build a complex robot to play a new game with a strict 6 week time limit and then compete with that robot in local and regional competitions. Outside of competitions, the team focuses on building technical and 21st century communications, collaboration and leadership skills for team members. In addition, the LigerBots serve as the focal point for project-based learning, participating in STEM outreach in the community to inspire others. Team activities include mentoring younger students in FIRST LEGO League (FLL), appearing at science fairs and community events, and corporate tech events as well as hosting FLL competitions and maker fairs to inspire younger students and the entire community. The LigerBots are proud of the team they have built in the last ten years&mdash;garnering recognition for success in developing a range of skills, from Engineering Inspiration to Imagery to Entrepreneurship to Gracious Professionalism. To support the team, use one of the &ldquo;Donate&rdquo; buttons above, message the team on Facebook, or email <a href="/cdn-cgi/l/email-protection#cbb8bba4a5b8a4b9e6b9aea7aabfa2a4a5b88ba7a2acaeb9a9a4bfb8e5a4b9ac"><span class="__cf_email__" data-cfemail="7102011e1f021e035c03141d1005181e1f02311d18161403131e05025f1e0316">[email&nbsp;protected]</span></a> if you are a corporate sponsor.
+</p>
+<div class="level4-heading">Why Your Company Should Sponsor the LigerBots</div>
+<p>Now's your chance to support the future engineers and entrepreneurs of Massachusetts. FIRST FRC is a fantastic program promoting STEM education, but building the robot and attending the competitions is quite expensive. Please consider donating to support the LigerBots in their return to the World Championships.</p>
+<p>&nbsp;</p>
+<div class="level4-heading">How To Sponsor the LigerBots</div>
+<p>Donations are handled through the Liger Alliance, which is the LigerBots' parent non-profit organization. The Liger Alliance is registered as a 501(c)3, although full approval from the IRS is still in process.</p>
+<p>The best way to make a donation to the team is to send a check to: LigerBots, P.O. Box 66171, Auburndale, MA 02466. Please make the check out to "Liger Alliance," and write "LigerBots" on the check memo line to designate the donation for the LigerBots. If you prefer, you can also donate to us via the PayPal link on this page.</p>
+<!-- *The Liger Alliance is registered as a 501(c)(3) non-profit organization. Contributions to the Newton Schools Foundation are tax-deductible to the extent permitted by law. -->
+<div class="level4-heading" style="margin-bottom: -25px;">The Benefits of Sponsorship</div>
+<p><img src="/images/sponsor_levels_2023.svg" width="100%" /></p>
+<div class="level4-heading">Sponsor Resources</div>
+<p>
+  What you get when you sponsor us: <a href="/docs/2024/Sponsor_Benefits_Flyer_2024.pdf" target="_blank" rel="noopener noreferrer">2023-24 Sponsor Benefits Flyer</a><br />
+  Benefits for individual donors:<a href="/docs/2024/Individual_Donor_Benefits_Flyer_2024.pdf" target="_blank" rel="noopener noreferrer">2023-24 Individual Donor Benefits Flyer</a><br />
+  General information about the LigerBots:<a href="/docs/2023/ligerbots_outreach_2023.pdf" target="_blank" rel="noopener noreferrer">2022-23 Outreach Flyer</a><br />Fun facts about the team and our budget:<a href="/docs/2024/ligerbots_infographic_2024.pdf" target="_blank" rel="noopener noreferrer">LigerBots Infographic</a><br />
+  Information about the team:<a href="/docs/2024/LigerBots_Team_Booklet_2024.pdf" target="_blank" rel="noopener noreferrer">LigerBots Team Booklet 2024</a><br />
+  Information about our robot:<a href="/docs/2024/LigerBots_Robot_Flyer_2024.pdf" target="_blank" rel="noopener noreferrer">LigerBots Robot Flyer 2024</a><br />
+  Using our logos:<a href="/docs/2022/LigerBots_Branding_Standards_2022.pdf" target="_blank" rel="noopener noreferrer">LigerBots Branding Standards</a><br />
+  LigerBots Logo Collection:<a href="/docs/2019/LigerBots_Logos_2019.zip" target="_blank" rel="noopener noreferrer">LigerBots Logos</a>
+</p>
