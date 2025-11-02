4f3cbfa43857067c3b7a3c1d6689198a0200b1c0
DeletedWasTaken <81261795+DeletedWasTaken@users.noreply.github.com>
2025-11-02 02:22:27 -0500

    began carpool ride selection display

--- src/lib/components/CarpoolTrip.svelte
+++ src/lib/components/CarpoolTrip.svelte
@@ -3,6 +3,13 @@
 
     /** @type {import('$lib/server/trip').Trip} */
     export let trip;
+
+    console.log(trip);
+
+    //@ts-ignore
+    export let rides = trip.item.rides
+
+    
 </script>
 <div>
     {#if trip}
@@ -11,6 +18,21 @@
         <span style="flex-basis: 100%;">Date: {item.departs_on}</span>
         <span>Departs at {item.departs_at}</span>
         <span>Arrives at {item.arrives_at}</span>
+        {#each rides as ride}
+	        {@const { item } = ride}
+	        {@const driver = item.ride.name}
+	        {@const { seats } = item.ride}
+	        {@const remaining = seats - item.riders_func.count}
+	        {@const seatDisplay = remaining > 0 ? `${remaining}/${seats} Seats Remaining` : 'Full'}
+
+	        <div style="flex-basis: 100%; display: flex; align-items: center; width: 100%;">
+		        <span style="flex: 1; text-align: left;">{driver} – {seatDisplay}</span>
+		        <span style="flex: 0 0 1rem; text-align: right;">select</span>
+	        </div>
+        {/each}
+
+
+        
     {:else}
         <p>Opt out of this trip</p>
     {/if}
